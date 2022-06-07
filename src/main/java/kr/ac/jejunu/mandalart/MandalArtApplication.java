d6ecc6c3d8ac8e5293cb74449394daceb7ad8cbc
ksg0605 <ksg0605@gmail.com>
2022-06-08 00:48:38 +0900

    Initial Commit

---
+++ src/main/java/kr/ac/jejunu/mandalart/MandalArtApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.mandalart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandalArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandalArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/jejunu/mandalart/MandalArtApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.mandalart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandalArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
