908b80fbb03eceac5d9a4302390b32dc1a872746
ksg0605 <ksg0605@gmail.com>
2022-06-08 16:28:51 +0900

    add BoardDao

---
+++ src/main/java/kr/ac/jejunu/mandalart/dao/BoardDao.java
@@ -0,0 +1,12 @@
+package kr.ac.jejunu.mandalart.dao;
+
+import kr.ac.jejunu.mandalart.Board;
+
+public class BoardDao {
+    public void registration(Board board) {
+    }
+
+    public Board findById(Integer id) {
+        return null;
+    }
+}
