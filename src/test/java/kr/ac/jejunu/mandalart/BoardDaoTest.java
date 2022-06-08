f035609c547c733cfff297ac421e05602c773047
ksg0605 <ksg0605@gmail.com>
2022-06-08 16:29:02 +0900

    add findById

--- src/test/java/kr/ac/jejunu/mandalart/BoardDaoTest.java
+++ src/test/java/kr/ac/jejunu/mandalart/BoardDaoTest.java
@@ -1,20 +1,37 @@
 package kr.ac.jejunu.mandalart;
 
+import kr.ac.jejunu.mandalart.dao.BoardDao;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 
 public class BoardDaoTest {
+    private static BoardDao boardDao;
+
+    @Test
+    public void findById() {
+        Integer id = 1;
+        String purpose = "목표 달성";
+
+        Board board = boardDao.findById(id);
+
+        assertThat(board.getId(), is(id));
+        assertThat(board.getPurpose(), is(purpose));
+    }
+
     @Test
     public void registration() {
-        Integer id = 1;
         String purpose = "목표 설정";
 
-        BoardDao boardDao = new BoardDao();
-        Board board = boardDao.registration();
+        Board board = new Board();
+        board.setPurpose(purpose);
+        boardDao.registration(board);
 
-        assertThat(board.setId(), is(id));
-        assertThat(board.setPurpose(), is(purpose));
+        assertThat(board.getId(), greaterThan(0));
+
+        Board registeredBoard = boardDao.findById(board.getId());
+        assertThat(registeredBoard.getPurpose(), is(board.getPurpose()));
     }
 }
