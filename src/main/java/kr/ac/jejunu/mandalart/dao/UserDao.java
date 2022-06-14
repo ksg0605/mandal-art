5b6eafb55271c7b7d705816d86eab8926d498ee9
ksg0605 <ksg0605@gmail.com>
2022-06-14 14:29:02 +0900

    add UserDao

---
+++ src/main/java/kr/ac/jejunu/mandalart/dao/UserDao.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.mandalart.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserDao {
+
+    public User findById(Integer id) {
+        return null;
+    }
+}
