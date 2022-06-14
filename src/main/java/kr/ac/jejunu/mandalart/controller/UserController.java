dea1f431d8ddc7f35f7c1938e083590026d32d21
ksg0605 <ksg0605@gmail.com>
2022-06-14 16:35:27 +0900

    add UserController

---
+++ src/main/java/kr/ac/jejunu/mandalart/controller/UserController.java
@@ -0,0 +1,35 @@
+package kr.ac.jejunu.mandalart.controller;
+
+import kr.ac.jejunu.mandalart.dao.UserDao;
+import kr.ac.jejunu.mandalart.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserDao userDao;
+
+    @GetMapping("/list")
+    public List<User> list() {
+        return userDao.findAll();
+    }
+
+    @RequestMapping(value = "save", method = {RequestMethod.POST, RequestMethod.PUT})
+    public User add(@RequestBody User user) {
+        return userDao.save(user);
+    }
+
+    @GetMapping("get/{id}")
+    public User get(@PathVariable Integer id) {
+        return userDao.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable Integer id) {
+        userDao.delete(userDao.findById(id).orElse(null));
+    }
+}
