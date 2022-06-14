41ef0d5f38549a9f67c0decdf953414bece2cdae
ksg0605 <ksg0605@gmail.com>
2022-06-14 23:29:54 +0900

    add BoardController

---
+++ src/main/java/kr/ac/jejunu/mandalart/controller/BoardController.java
@@ -0,0 +1,28 @@
+package kr.ac.jejunu.mandalart.controller;
+
+import kr.ac.jejunu.mandalart.dao.BoardDao;
+import kr.ac.jejunu.mandalart.model.Board;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final BoardDao boardDao;
+
+    @RequestMapping(value = "save", method = {RequestMethod.POST, RequestMethod.PUT})
+    public Board plan(@RequestBody Board board) {
+        return boardDao.save(board);
+    }
+
+    @GetMapping("get/{id}")
+    public Board get(@PathVariable Integer id) {
+        return boardDao.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable Integer id) {
+        boardDao.delete(boardDao.findById(id).orElse(null));
+    }
+}
