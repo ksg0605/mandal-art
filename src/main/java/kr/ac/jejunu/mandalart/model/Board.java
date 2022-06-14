402d1d4bc1a4df39596226986f8bed9cdab99332
ksg0605 <ksg0605@gmail.com>
2022-06-14 16:31:44 +0900

    update models

---
+++ src/main/java/kr/ac/jejunu/mandalart/model/Board.java
@@ -0,0 +1,19 @@
+package kr.ac.jejunu.mandalart.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity(name = "boards")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String purpose;
+
+}
